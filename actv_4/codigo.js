3cefc71f618f507860d45ee7ef49a09cebdee03b
Jhonnys Padilla <79125202+JhonnysPd@users.noreply.github.com>
2021-03-20 12:26:53 -0500

    add

--- actv_4/codigo.js
+++ actv_4/codigo.js
@@ -11,13 +11,24 @@ function mostrarListadoNumeros(){
     var salida1='';
     var contar = arrayNumeros.length;
     var suma= sumaElementos();
+    var elemMayor= Math.max.apply(null,arrayNumeros);
+    var elemMen= Math.min.apply(null,arrayNumeros);
+    var numerosImpares= numImpares();
+    var numPar= 0;
+    var multCin= 0;
 
     for(i=0; i< contar; i++)
     salida+='<tr><td>'+arrayNumeros[i]+'</td></tr>';
     document.getElementById('cuerpo').innerHTML=salida;
-    salida1+= '<tr><td>'+'Cantidad de Elementos'+ '</td><td>'+ arrayNumeros.length+'</td></tr>';
+    salida1+= '<tr><td>'+'Cantidad de Elementos'+ '</td><td>'+ contar+'</td></tr>';
     salida1+= '<tr><td>'+'Suma de Elementos'+ '</td><td>'+ suma+'</td></tr>';
     salida1+= '<tr><td>'+'Promedio'+'</td><td>'+suma/contar +'</td></tr>';
+    salida1+= '<tr><td>'+ 'Elemento Mayor' +'</td><td>'+ elemMayor +'</td></tr>';
+    salida1+= '<tr><td>'+'Elemento Menor'+'</td><td>'+elemMen +'</td></tr>';
+    salida1+= '<tr><td>'+'Números Impares'+'</td><td>'+ "" +'</td></tr>';
+    salida1+= '<tr><td>'+'Números Pares'+'</td><td>'+"" +'</td></tr>';
+
+
     document.getElementById('otroCuerpo').innerHTML=salida1;
 }
 function sumaElementos(){
